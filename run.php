f4a449dc4d0da0d42e019f844256f0df2f0cc66a
Matt Light <matt.light@lightdatasys.com>
2014-01-01 22:00:01 -0800

    Update examples in run.php

--- run.php
+++ run.php
@@ -3,9 +3,25 @@
 require_once __DIR__ . '/vendor/autoload.php';
 
 use Lstr\Github\Api\Api;
+use Lstr\Github\Api\Exception as ApiException;
 
 $config = require 'config.local.php';
 
 $api = new Api($config['users']['lightster']);
 
-var_dump($api->getUserRepos());
+try {
+    foreach ($api->getUserRepos()->getData() as $repo) {
+        echo "{$repo['full_name']}\n";
+    }
+    foreach ($config['users']['lightster']['organizations'] as $organization) {
+        echo "\n\n";
+        echo "== {$organization} ==\n";
+        foreach ($api->getOrgsRepos(array('organization' => $organization))->getData() as $repo) {
+            echo "{$repo['full_name']}\n";
+        }
+    }
+} catch (ApiException $ex) {
+    echo "ApiException (" . get_class($ex) .  "): " . $ex->getMessage() . "\n";
+} catch (Exception $ex) {
+    echo "Exception: (" . get_class($ex) .  "): " . $ex->getMessage() . "\n";
+}
