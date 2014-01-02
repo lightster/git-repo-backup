951615d1327c1eb48daa5e4dda6e54aef09c6970
Matt Light <matt.light@lightdatasys.com>
2014-01-01 21:58:40 -0800

    Add re-usable way to check args and set default values

--- src/Lstr/Github/Api/MethodCall.php
+++ src/Lstr/Github/Api/MethodCall.php
@@ -9,7 +9,7 @@ abstract class MethodCall
 
 
 
-    public function __construct(Caller $caller, array $args = null)
+    public function __construct(Caller $caller, array $args = array())
     {
         $this->caller = $caller;
         $this->args   = $args;
@@ -27,6 +27,42 @@ abstract class MethodCall
     protected function getArgs()
     {
         return $this->args;
+    }
+
+
+
+    protected function checkArguments(
+        array $required_args,
+        array $optional_args
+    ) {
+        $args = array();
+
+        foreach ($required_args as $key) {
+            if (!array_key_exists($key, $this->args)) {
+                throw new Exception("Argument '{$key}' is required but not provided");
+            }
+
+            $args[$key] = $this->args[$key];
+        }
+
+        foreach ($optional_args as $key => $value) {
+            $use_default = true;
+            // If the key is numeric, then '$value' is actually the argument
+            // name, there is no default value, and the argument should not
+            // be returned unless the value is in $this->args
+            if (is_int($key)) {
+                $use_default = false;
+                $key         = $value;
+            }
+
+            if (array_key_exists($key, $this->args)) {
+                $args[$key] = $this->args[$key];
+            } else if ($use_default) {
+                $args[$key] = $value;
+            }
+        }
+
+        return $args;
     }
 
 
