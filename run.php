55cd85afc14a1c5593e5b93c2d4f1a447e0852d4
Matt Light <matt.light@lightdatasys.com>
2014-01-02 23:42:28 -0800

    Add RepositoryMirror class to setup/update git repo mirrors

--- run.php
+++ run.php
@@ -5,23 +5,121 @@ require_once __DIR__ . '/vendor/autoload.php';
 use Lstr\Github\Api\Api;
 use Lstr\Github\Api\Exception as ApiException;
 
+use Lstr\Git\RepositoryMirror;
+
 $config = require 'config.local.php';
 
-$api = new Api($config['users']['lightster']);
-
 try {
-    foreach ($api->getUserRepos()->getData() as $repo) {
-        echo "{$repo['full_name']}\n";
-    }
-    foreach ($api->getUserOrgs()->getData() as $user_org) {
-        $organization = $user_org['login'];
+    $repos = array();
 
-        echo "\n\n";
-        echo "== {$organization} ==\n";
-        foreach ($api->getOrgsRepos(array('organization' => $organization))->getData() as $repo) {
-            echo "{$repo['full_name']}\n";
+    foreach ($config['github'] as $config_name => $backup_set) {
+        $api = new Api($backup_set);
+
+        $accessible_repos = array();
+        foreach ($api->getUserRepos()->getData() as $repo) {
+            $accessible_repos[$repo['full_name']] = $repo;
+        }
+        foreach ($api->getUserOrgs()->getData() as $user_org) {
+            $organization = $user_org['login'];
+
+            foreach ($api->getOrgsRepos(array('organization' => $organization))->getData() as $repo) {
+                $accessible_repos[$repo['full_name']] = $repo;
+            }
+        }
+
+        foreach ($backup_set['repos'] as $pattern_name => $repo_pattern) {
+            $repo_names = array_keys($accessible_repos);
+
+            if (!empty($repo_pattern['include_pattern'])) {
+                $repo_names = preg_grep(
+                    $repo_pattern['include_pattern'],
+                    $repo_names
+                );
+            }
+            if (!empty($repo_pattern['exclude_pattern'])) {
+                $repo_names = preg_grep(
+                    $repo_pattern['exclude_pattern'],
+                    $repo_names,
+                    PREG_GREP_INVERT
+                );
+            }
+
+            if (!empty($repo_pattern['exclude_forks'])) {
+                $repo_names = array_filter($repo_names, function ($repo_name) use ($accessible_repos) {
+                    return !$accessible_repos[$repo_name]['fork'];
+                });
+            }
+
+            if (empty($repo_names)) {
+                $message = "Pattern matched no repos:\n"
+                    . "'{$config_name}:{$pattern_name}' => "
+                    . var_export($repo_pattern, true)
+                    . "\n\n";
+                fwrite(STDERR, $message);
+            }
+
+            foreach ($repo_names as $repo_name) {
+                if (!isset($repos[$repo_name])) {
+                    $repos[$repo_name] = array(
+                        'repo_name'    => $repo_name,
+                        'config_name'  => $config_name,
+                        'backup_set'   => $backup_set,
+                        'pattern_name' => $pattern_name,
+                        'repo_pattern' => $repo_pattern,
+                    );
+                } else {
+                    $other_match = $repos[$repo_name];
+                    $message = "Repo '{$repo_name}' already matched by other pattern set.\n"
+                        . "'{$other_match['config_name']}:{$other_match['pattern_name']}' => "
+                        . var_export($other_match['repo_pattern'], true)
+                        . "\n"
+                        . "'{$config_name}:{$pattern_name}' => "
+                        . var_export($repo_pattern, true)
+                        . "\n\n";
+                    fwrite(STDERR, $message);
+                }
+            }
         }
     }
+
+    foreach ($repos as $repo_name => $repo) {
+        $destination_location = str_replace(
+            '{{REPO_NAME}}',
+            $repo_name,
+            $repo['backup_set']['destination_location']
+        );
+        $repo_mirror = new RepositoryMirror($destination_location);
+
+        try {
+            $remote_url = str_replace(
+                '{{REPO_NAME}}',
+                $repo_name,
+                $repo['backup_set']['clone_url']
+            );
+
+            echo "Is '{$destination_location}' setup? ";
+            if ($repo_mirror->isRemoteSetup()) {
+                echo "Yes\n";
+                echo "Does existing remote match '{$remote_url}'? ";
+                $repo_mirror->checkRemoteUrlAgainst($remote_url);
+                echo "Yes\n";
+                echo "Running update ... ";
+                $repo_mirror->update();
+                echo "done\n";
+            } else {
+                echo "No\n";
+                echo "Setting up using '{$remote_url}' ... ";
+                $repo_mirror->setup($remote_url);
+                echo "done\n";
+            }
+
+            echo "\n";
+        } catch (Exception $ex) {
+            echo "An exception occurred while mirroring repository '{$repo_name}':\n";
+            echo $ex->getMessage() . "\n\n\n\n";
+        }
+    }
+    //echo var_export(array_keys($repos), true) . "\n";
 } catch (ApiException $ex) {
     echo "ApiException (" . get_class($ex) .  "): " . $ex->getMessage() . "\n";
 } catch (Exception $ex) {
---
+++ src/Lstr/Git/RepositoryMirror.php
@@ -0,0 +1,166 @@
+<?php
+
+namespace Lstr\Git;
+
+use Exception;
+
+class RepositoryMirror
+{
+    private $destination_path;
+
+    private $remote_url;
+
+
+
+    public function __construct($destination_path)
+    {
+        $this->destination_path = $destination_path;
+        $this->remote_url       = null;
+    }
+
+
+
+    public function isRemoteSetup()
+    {
+        if (!is_dir($this->destination_path)) {
+            return false;
+        }
+
+        try {
+            if ($this->getRemoteUrl()) {
+                return true;
+            }
+        } catch (Exception $ex) {
+            // do nothing; an exception mean
+        }
+
+        return false;
+    }
+
+
+
+    public function getRemoteUrl()
+    {
+        if (null !== $this->remote_url) {
+            return $this->remote_url;
+        }
+
+        $command = "git --git-dir "
+            . escapeshellarg($this->destination_path)
+            . " ls-remote --get-url 2>&1";
+        $results = $this->runCommand($command);
+
+        // if there is a non-zero exit code or the exit code was not set,
+        // an error occurred
+        if ($results['exit_code'] || null === $results['exit_code']) {
+            throw $this->generateException(
+                "The remote URL could not be determined.",
+                $results
+            );
+        }
+
+        return $results['last_line'];
+    }
+
+
+
+    public function checkRemoteUrlAgainst($remote_url)
+    {
+        $existing_remote_url = $this->getRemoteUrl();
+
+        if ($remote_url !== $existing_remote_url) {
+            $message = "The existing mirror's remote URL and the "
+                . "provided remote URL do not match.\n"
+                . "Existing mirror remote URL:\n"
+                . "  {$existing_remote_url}\n"
+                . "Provided remote URL:\n"
+                . "  {$remote_url}\n\n";
+            throw new Exception($message);
+        }
+
+        return true;
+    }
+
+
+
+    public function setup($remote_url)
+    {
+        $command = "git clone -q --mirror "
+            . escapeshellarg($remote_url)
+            . " "
+            . escapeshellarg($this->destination_path)
+            . " 2>&1";
+        echo "Running:\n  {$command}\n...";
+        $results = $this->runCommand($command);
+
+        // if there is a non-zero exit code or the exit code was not set,
+        // an error occurred
+        if ($results['exit_code'] || null === $results['exit_code']) {
+            throw $this->generateException(
+                "The remote repository could not be mirrored.",
+                $results
+            );
+        }
+        echo "done\n";
+
+        $this->checkRemoteUrlAgainst($remote_url);
+
+        return true;
+    }
+
+
+
+    public function update()
+    {
+        $command = "git --git-dir "
+            . escapeshellarg($this->destination_path)
+            . " remote update"
+            . " 2>&1";
+        $results = $this->runCommand($command);
+
+        // if there is a non-zero exit code or the exit code was not set,
+        // an error occurred
+        if ($results['exit_code'] || null === $results['exit_code']) {
+            throw $this->generateException(
+                "The remote repository could not be mirrored.",
+                $results
+            );
+        }
+
+        return true;
+    }
+
+
+
+    protected function runCommand($command)
+    {
+        $results = array(
+            'command'   => $command,
+            'last_line' => null,
+            'output'    => null,
+            'exit_code' => null,
+        );
+        $results['last_line'] = exec(
+            $command,
+            $results['output'],
+            $results['exit_code']
+        );
+
+        return $results;
+    }
+
+
+
+    protected function generateException($message, array $results)
+    {
+        $ex_message = "{$message}\n"
+            . "Command ran:\n"
+            . "  {$results['command']}\n"
+            . "Exit code:\n"
+            . "  {$results['exit_code']}\n"
+            . "Output:\n"
+            . "  " . implode("\n  ", $results['output'])
+            . "\n\n";
+        return new Exception($ex_message);
+    }
+}
