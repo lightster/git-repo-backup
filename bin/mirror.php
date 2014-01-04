d5e0c6a8daca753ffaaacc84d64a1f9e985d28d2
Matt Light <matt.light@lightdatasys.com>
2014-01-04 13:22:57 -0800

    Move run.php to bin/mirror.php

--- bin/mirror.php
+++ bin/mirror.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 use Lstr\Git\RepositoryMirrorer\Logger;
 use Lstr\Git\RepositoryMirrorer\RepositoryMirrorer;
