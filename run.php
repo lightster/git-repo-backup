a3e35416e196a86f50123659617196fcd97573b7
Matt Light <matt.light@lightdatasys.com>
2014-01-04 00:46:59 -0800

    Update run.php to use RepositoryListLoader

--- run.php
+++ run.php
@@ -3,6 +3,8 @@
 require_once __DIR__ . '/vendor/autoload.php';
 
 use Lstr\Git\RepositoryMirror;
+use Lstr\Git\RepositoryMirrorer\Logger;
+use Lstr\Git\RepositoryMirrorer\RepositoryListLoader;
 use Lstr\Github\Api\Exception as ApiException;
 use Lstr\Github\Gateway\Github;
 
@@ -11,104 +13,39 @@ $config = require 'config.local.php';
 try {
     $repos = array();
 
-    foreach ($config['github'] as $config_name => $backup_set) {
-        $github           = new Github($backup_set);
-        $auth_user        = $github->getAuthenticatedUser();
-        $accessible_repos = $auth_user->getAllRepositories();
+    $logger          = new Logger(true);
+    $list_loader     = new RepositoryListLoader($config['repository_lists']);
+    $list_loader->setLogger($logger);
+    $repository_list = $list_loader->getRepositoryList();
 
-        foreach ($backup_set['repos'] as $pattern_name => $repo_pattern) {
-            $repo_names = array_keys($accessible_repos);
-
-            if (!empty($repo_pattern['include_pattern'])) {
-                $repo_names = preg_grep(
-                    $repo_pattern['include_pattern'],
-                    $repo_names
-                );
-            }
-            if (!empty($repo_pattern['exclude_pattern'])) {
-                $repo_names = preg_grep(
-                    $repo_pattern['exclude_pattern'],
-                    $repo_names,
-                    PREG_GREP_INVERT
-                );
-            }
-
-            if (!empty($repo_pattern['exclude_forks'])) {
-                $repo_names = array_filter($repo_names, function ($repo_name) use ($accessible_repos) {
-                    return !$accessible_repos[$repo_name]['fork'];
-                });
-            }
-
-            if (empty($repo_names)) {
-                $message = "Pattern matched no repos:\n"
-                    . "'{$config_name}:{$pattern_name}' => "
-                    . var_export($repo_pattern, true)
-                    . "\n\n";
-                fwrite(STDERR, $message);
-            }
-
-            foreach ($repo_names as $repo_name) {
-                if (!isset($repos[$repo_name])) {
-                    $repos[$repo_name] = array(
-                        'repo_name'    => $repo_name,
-                        'config_name'  => $config_name,
-                        'backup_set'   => $backup_set,
-                        'pattern_name' => $pattern_name,
-                        'repo_pattern' => $repo_pattern,
-                    );
-                } else {
-                    $other_match = $repos[$repo_name];
-                    $message = "Repo '{$repo_name}' already matched by other pattern set.\n"
-                        . "'{$other_match['config_name']}:{$other_match['pattern_name']}' => "
-                        . var_export($other_match['repo_pattern'], true)
-                        . "\n"
-                        . "'{$config_name}:{$pattern_name}' => "
-                        . var_export($repo_pattern, true)
-                        . "\n\n";
-                    fwrite(STDERR, $message);
-                }
-            }
-        }
-    }
-
-    foreach ($repos as $repo_name => $repo) {
-        $destination_location = str_replace(
-            '{{REPO_NAME}}',
-            $repo_name,
-            $repo['backup_set']['destination_location']
-        );
-        $repo_mirror = new RepositoryMirror($destination_location);
+    foreach ($repository_list->getRepositories() as $repo) {
+        $repo_mirror = new RepositoryMirror($repo['destination']);
 
         try {
-            $remote_url = str_replace(
-                '{{REPO_NAME}}',
-                $repo_name,
-                $repo['backup_set']['clone_url']
-            );
+            $remote_url = $repo['remote_url'];
 
-            echo "Is '{$destination_location}' setup? ";
+            $logger->debug("Is '{$repo['destination']}' setup?");
             if ($repo_mirror->isRemoteSetup()) {
-                echo "Yes\n";
-                echo "Does existing remote match '{$remote_url}'? ";
+                $logger->debug("  Yes");
+                $logger->debug("Does existing remote match '{$remote_url}'?");
                 $repo_mirror->checkRemoteUrlAgainst($remote_url);
-                echo "Yes\n";
-                echo "Running update ... ";
+                $logger->debug("  Yes");
+                $logger->debug("Running update ...");
                 $repo_mirror->update();
-                echo "done\n";
+                $logger->debug("  done");
             } else {
-                echo "No\n";
-                echo "Setting up using '{$remote_url}' ... ";
+                $logger->debug("  No");
+                $logger->debug("Setting up using '{$remote_url}' ... ");
                 $repo_mirror->setup($remote_url);
-                echo "done\n";
+                $logger->debug("  done");
             }
 
             echo "\n";
         } catch (Exception $ex) {
-            echo "An exception occurred while mirroring repository '{$repo_name}':\n";
-            echo $ex->getMessage() . "\n\n\n\n";
+            $logger->alert("An exception occurred while mirroring repository '{$repo_name}':");
+            $logger->alert($ex->getMessage() . "\n\n\n");
         }
     }
-    //echo var_export(array_keys($repos), true) . "\n";
 } catch (ApiException $ex) {
     echo "ApiException (" . get_class($ex) .  "): " . $ex->getMessage() . "\n";
 } catch (Exception $ex) {
