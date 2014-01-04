57da57ba945b28dab450eb319f723eaa9723a1d9
Matt Light <matt.light@lightdatasys.com>
2014-01-04 13:20:34 -0800

    Update run.php to allow config path to be passed in

--- run.php
+++ run.php
@@ -2,13 +2,32 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
+use Lstr\Git\RepositoryMirrorer\Logger;
 use Lstr\Git\RepositoryMirrorer\RepositoryMirrorer;
 use Lstr\Github\Api\Exception as ApiException;
 
-$config = require 'config.local.php';
+if (!isset($argv[1])) {
+    echo "Usage: {$argv[0]} path-to-config\n";
+    exit(100);
+}
+
+$config_file = $argv[1];
+if (!is_file($config_file)) {
+    echo "Could not locate config file: {$config_file}\n";
+    exit(101);
+}
+
+$config = require $config_file;
+if (!is_array($config)) {
+    echo "\nConfig file did not return an array: {$config_file}\n";
+    var_export($config);
+    exit(102);
+}
 
 try {
     $mirrorer  = new RepositoryMirrorer($config['repository_lists']);
+    $logger    = new Logger(!empty($config['verbose']));
+    $mirrorer->setLogger($logger);
     $exit_code = $mirrorer->run();
 
     exit($exit_code);
